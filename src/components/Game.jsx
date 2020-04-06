18c0523da8ff0cc175787122099492a6108a1662
Max Han <mwellh84@gmail.com>
2020-04-06 16:45:09 -0400

    Feature(game): add game methods

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -16,6 +16,37 @@ class Game extends Component {
     };
   }
 
+  initBoard = (w, h) => {
+    const board = [];
+    for (let r = 0; r < h; r++) {
+      board.push([]);
+      for (let c = 0; c < w; c++) {
+        board[r][c] = {
+          row: r,
+          col: c,
+          isMine: false,
+          isRevealed: false,
+          isFlagged: false,
+          mineCount: null,
+          setOffMine: false,
+        };
+      }
+    }
+    return board;
+  };
+
+  plantMines = (board, w, h, mines) => {
+    let count = 0;
+    while (count < mines) {
+      let row = Math.floor(Math.random() * h);
+      let col = Math.floor(Math.random() * w);
+      if (board[row][col].isMine === false) {
+        board[row][col].isMine = true;
+        count++;
+      }
+    }
+  };
+
   render() {
     const { board, gameStarted, gameOver, gameWon } = this.state;
     return (
