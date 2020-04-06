5c678b68f4f23135d4c36a9aae0fe72d4d5c3c65
Max Han <mwellh84@gmail.com>
2020-04-06 16:45:48 -0400

    Feature: setup board

---
+++ src/components/Board.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Tile from "./Tile";
+
+const Board = (props) => {
+  const { board, handleTile, mineCount } = props || { board: [] };
+  return (
+    <section>
+      <table className="board-container">
+        <tbody>
+          {!board.length && <h5>Pick a Game</h5>}
+          {board.length > 0 &&
+            board.map((row, r) => {
+              return (
+                <tr key={row + r}>
+                  {row.map((col, c) => {
+                    return (
+                      <Tile
+                        key={`${r}-${c}`}
+                        {...col}
+                        handleTile={handleTile}
+                        gameRows={board.length}
+                      />
+                    );
+                  })}
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default Board;
