d3898883c36b8af7ab458a0daf78fb6429b0b58e
Max Han <mwellh84@gmail.com>
2020-04-06 17:08:50 -0400

    Updates

---
+++ precache-manifest.df99fb38f8e4a9fa2ff1626806b8491c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1791465f64fb7cb61ee12429c69158fe",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "83f1bb8b4758fced5920",
+    "url": "/minesweeper/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1ebb746789bfa6cc2705",
+    "url": "/minesweeper/static/css/main.40d3ef25.chunk.css"
+  },
+  {
+    "revision": "83f1bb8b4758fced5920",
+    "url": "/minesweeper/static/js/2.ab9fbb33.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/minesweeper/static/js/2.ab9fbb33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ebb746789bfa6cc2705",
+    "url": "/minesweeper/static/js/main.a85f0ee3.chunk.js"
+  },
+  {
+    "revision": "c6dccd2f9e42058b62f6",
+    "url": "/minesweeper/static/js/runtime-main.c6b1b01c.js"
+  }
+]);
